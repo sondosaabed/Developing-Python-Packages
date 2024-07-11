1f19992ca36a6f7dd66b339e3ceccb3557a7ee6e
Sondos Ahmad Aabed <65151701+sondosaabed@users.noreply.github.com>
2024-07-11 18:38:04 +0300

    Use the flake8 to check with the style of ZEN

---
+++ Chapter-3-Increasing-Your-Package-Quality/absolute.py
@@ -0,0 +1,21 @@
+"""Main module."""
+
+
+def absolute_value(num):
+    """
+    Return the absolute value of the number
+    """
+    if num >= 0:
+        return num
+    else:
+        return -num
+
+
+## Before the flake8
+# """Main module."""
+
+# def absolute_value(num):
+#     """Return the absolute value of the number"""
+#     if num>=0: return num
+#     else:
+#     	return -num
