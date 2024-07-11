7b1cb851a7564471688092792d8c1368ebe3c6db
Sondos Ahmad Aabed <65151701+sondosaabed@users.noreply.github.com>
2024-07-11 19:19:12 +0300

    ignoring one error of flake8

---
+++ Chapter-3-Increasing-Your-Package-Quality/pythagoras.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def calculate_hypotenuse(side1, side2):
+    """Calculate the length of the hypotenuse."""
+    l = np.sqrt( side1**2 + side2**2 )  # ____: ____
+    return l
+
+### solution
+import numpy as np
+
+
+def calculate_hypotenuse(side1, side2):
+    """Calculate the length of the hypotenuse."""
+    l = np.sqrt( side1**2 + side2**2 )  # noqa: E741
+    return l
